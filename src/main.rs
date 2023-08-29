9bbd9c50fb6137fa901cfd3e86a2898d70470bda
felix-rojas <a01201946@tec.mx>
2023-08-29 15:03:29 -0600

    add flapping & player

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,44 @@
 use bracket_lib::prelude::*;
 
+const SCRREN_WIDTH: i32 = 800;
+const SCRREN_HEIGHT: i32 = 800;
+const FRAME_DURATION: i32 = 75.0;
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
+
 enum GameMode {
     Menu,
     Playing,
@@ -7,17 +46,23 @@ enum GameMode {
 }
 
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
 impl State {
     fn new() -> Self {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 
@@ -51,8 +96,21 @@ impl State {
         }
     }
 
-    fn play(&mut self, ctx: &mut BTerm) {
-        self.mode = GameMode::End;
+    fn play(&mut self, _ctx: &mut BTerm) {
+        ctx.cls_bg(GREY6);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press space to flap.");
+        if self.player.y > SCRREN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
 }
 
@@ -71,4 +129,3 @@ fn main() -> BError {
                                                                              // returns a BError
     main_loop(context, State::new())
 }
-
