80773d19850a466bb7bfca440b8b27091e47df99
felix-rojas <a01201946@tec.mx>
2023-08-29 14:37:43 -0600

    finish menu and game states

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,74 @@
 use bracket_lib::prelude::*;
 
-fn main() -> BError {
-
-struct State {
+enum GameMode {
+    Menu,
+    Playing,
+    End,
 }
 
-impl GameState for State {
-    fn tick(&mut self, ctx: &mut BTerm){
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1,1, "This is a bracket Terminal");
+        ctx.print_centered(5, "Welcome to flappy");
+        ctx.print_centered(8, "Play Game");
+        ctx.print_centered(9, "Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You died");
+        ctx.print_centered(8, "Play Game Again");
+        ctx.print_centered(9, "Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        self.mode = GameMode::End;
     }
 }
 
-    let context = BTermBuilder::simple80x50()
-        .with_title("Flappy")
-        .build()?; // return Result
-    // returns a BError
-    main_loop(context, State{})
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx),
+        }
+    }
 }
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50().with_title("Flappy").build()?; // return Result
+                                                                             // returns a BError
+    main_loop(context, State::new())
+}
+
